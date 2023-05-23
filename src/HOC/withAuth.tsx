f983c60ac665b00d1d2893647d1e5e428f160a80
frozengem397 <frozengem0123@gmail.com>
2023-05-23 11:58:15 -0700

    authPath

--- src/HOC/withAuth.tsx
+++ src/HOC/withAuth.tsx
@@ -1,10 +1,12 @@
 import React from "react"
 
+
 const withAuth = (WrappedComponent:any)=>{
+    
     return(props:any) => {
         const accessToken = localStorage.getItem("token");
         if(!accessToken){
-            window.location.replace("/KimFood/login");
+            window.location.replace("https://frozengem397.github.io/KimFood/login");
             return null;
         }
         return <WrappedComponent {...props} />
