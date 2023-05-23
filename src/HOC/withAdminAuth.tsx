b6b38138c0b67c1ba57d271f766ecea60a27e502
frozengem397 <frozengem0123@gmail.com>
2023-05-23 11:28:46 -0700

    update admin

--- src/HOC/withAdminAuth.tsx
+++ src/HOC/withAdminAuth.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import jwt_decode from "jwt-decode";
-import { useNavigate } from "react-router-dom";
+
 
 const withAdminAuth = (WrappedComponent:any)=>{
    
@@ -12,13 +12,16 @@ const withAdminAuth = (WrappedComponent:any)=>{
                 role:string;
             } = jwt_decode(accessToken);
             if(decode.role!=="admin"){
-                window.location.replace("/KimFood/accessDenied");
-                
+                setTimeout(()=>{
+                    window.location.replace("/KimFood/accessDenied");
+                })             
                 return null;
             }
         }
         else{
-            window.location.replace("/KimFood/login");
+            setTimeout(()=>{
+                window.location.replace("/KimFood/login");
+            })           
             return null;
         }
         
